c5a4c6e9273ae4cea2aa554601155203878dc689
Daniil Ladygin <95340198+DaniLadygin@users.noreply.github.com>
2022-04-12 18:53:20 +0300

    Add files via upload

---
+++ finalproject/app/src/redux/reducers/rootReducer.js
@@ -0,0 +1,15 @@
+
+
+import { combineReducers } from "redux";
+import { personReducer } from "./personReducer";
+import postsReducer from "./postsReducer";
+import { searchReducer } from "./searchReducer";
+
+const rootReducer = combineReducers({
+	posts: postsReducer,
+	search: searchReducer,
+	person: personReducer
+})
+
+
+export default rootReducer
